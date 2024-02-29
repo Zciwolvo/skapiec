77c4cf4e4cfd6c8c11502a878f183795df83e603
Rucol <rucki12@interia.pl>
2024-02-29 08:30:49 +0100

    2. Scraper 1 skońcozny

--- Scraper.py
+++ Scraper.py
@@ -10,15 +10,20 @@ def scrapping(fraza):
         products = soup.find_all('h2', title=True)
         prices = soup.find_all('span', class_='price')
         wewURLs = soup.find_all('a', class_='product-box-narrow__title')
+        photos = soup.find_all('div', class_ = 'product-box-narrow__photo-box-image')
+        zewURLs = soup.find_all('a', class_ = 'button button--primary button--md button--fw-500 product-box-narrow__button product-box-narrow__button--shop')
 
         if len(products) == len(prices):
             products_info = []
 
-            for product, price, wewURL in zip(products, prices, wewURLs):
+            for product, price, wewURL, photo, zewURL in zip(products, prices, wewURLs, photos, zewURLs):
                 name = product['title']
                 price_value = price.get_text().replace('\xa0zł', '').replace(',', '.')
                 skURL = wewURL['href'] if wewURL else None
-                products_info.append({'name': name, 'price': price_value, 'URL wewnętrzne': skURL})
+                photo = photo.find('img')['src'] if photo.find('img') else None
+                sklepURL = zewURL['href'] if zewURL else None 
+                products_info.append({'name': name, 'price': price_value, 'URL wewnętrzne': skURL, 'photo' : photo, 'URL zewnętrzne': sklepURL})
+
 
             return products_info
         else:
@@ -32,6 +37,7 @@ result = scrapping(search_phrase)
 if result:
     for item in result:
         skURL = item['URL wewnętrzne'] if item['URL wewnętrzne'] else "Brak"
-        print(f"Nazwa: {item['name']}, Cena: {item['price']} zł, URL wewnętrzne: {"skapiec.pl"+skURL}")
+        sklepURL = item['URL zewnętrzne'] if item['URL zewnętrzne'] else "brak"
+        print(f"Nazwa: {item['name']}, Cena: {item['price']} zł, URL wewnętrzne: {"skapiec.pl"+skURL}, URL zewnętrzne: {sklepURL}, Zdjecie: {item['photo']}\n")
 else:
     print("Brak wyników")
